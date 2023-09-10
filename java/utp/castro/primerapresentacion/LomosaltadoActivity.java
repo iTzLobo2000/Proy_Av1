f52f9f09a29430f7af780ba864b62e91f53ccb9b
iTzLobo2000 <133928633+iTzLobo2000@users.noreply.github.com>
2023-09-10 17:01:36 -0500

    Update LomosaltadoActivity.java

--- java/utp/castro/primerapresentacion/LomosaltadoActivity.java
+++ java/utp/castro/primerapresentacion/LomosaltadoActivity.java
@@ -2,13 +2,28 @@ package utp.castro.primerapresentacion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LomosaltadoActivity extends AppCompatActivity {
+
+    Button btnatras;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lomosaltado);
+
+        btnatras=findViewById(R.id.btnatras);
+
+        btnatras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LomosaltadoActivity.this,PlatosregistradosActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
