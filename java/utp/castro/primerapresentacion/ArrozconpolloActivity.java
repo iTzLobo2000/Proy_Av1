24502250469c799dfeac4a69380b1b052e24b435
iTzLobo2000 <133928633+iTzLobo2000@users.noreply.github.com>
2023-09-10 17:02:01 -0500

    Update ArrozconpolloActivity.java

--- java/utp/castro/primerapresentacion/ArrozconpolloActivity.java
+++ java/utp/castro/primerapresentacion/ArrozconpolloActivity.java
@@ -2,13 +2,30 @@ package utp.castro.primerapresentacion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ArrozconpolloActivity extends AppCompatActivity {
 
+Button btnatras;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_arrozconpollo);
+
+        btnatras=findViewById(R.id.btnatras);
+
+        btnatras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ArrozconpolloActivity.this,PlatosregistradosActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
